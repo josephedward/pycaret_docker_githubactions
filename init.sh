ffe15fae74f50ed635d72808848dbb3d3f82fec3
JoeV <josephedwardwork@gmail.com>
2020-08-05 21:51:31 -0400

    reinitialized, removed venv

---
+++ init.sh
@@ -0,0 +1,16 @@
+python3 -m venv venv
+source "./venv/bin/activate"
+#pip install -r './resources/requirements.txt'
+# export PYTHONPATH=/opt/anaconda3/lib/python3.7/site-packages
+
+pip install --upgrade pip
+pip install flask
+pip install pandas
+pip install bs4
+pip install splinter
+pip install nltk
+pip install sklearn
+pip install gensim
+pip install matplotlib
+pip install pycaret
+
